aaf7b532d7fe7dd662b1fd89ce77f90a3aacf52d
Johany Florez <johanypenaflorez@Johanys-Laptop.local>
2025-09-29 08:53:11 -0500

    changes

--- PLATZI/EJ1.py
+++
@@ -1,3 +0,0 @@
-numeros = list(range(11))
-cuadrados = list(map(lambda x: x ** 2, numeros))
-print("Cuadrados:", cuadrados)
--- PLATZI/test.py
+++
@@ -1,9 +0,0 @@
-a=-4
-
-
-if  a <0 :
-    print("negativot")
-else:
-    print("positvo")
-
-print(a<0)
